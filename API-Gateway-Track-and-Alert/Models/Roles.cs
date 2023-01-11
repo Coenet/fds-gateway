3546142ea8b2aedc3daec4458b449b205e3ba4f5
Coenet <99261323+Coenet@users.noreply.github.com>
2023-01-11 10:48:05 +0100

    create gateway

---
+++ API-Gateway-Track-and-Alert/Models/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace API_Gateway_Track_And_Alert.Models;
+
+[Flags]
+public enum Roles
+{
+    User = 1,
+    Admin = 2,
+    CollarManager = 4,
+    Researcher = 8
+}
---
+++ API-Gateway-Track-and-Alert/Program.cs
@@ -0,0 +1,50 @@
+using API_Gateway_Track_And_Alert.Helpers;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Kubernetes;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("secrets/appsettings.secrets.json", true);
+
+
+var securityKey = builder.Configuration.GetSection("Keys")["SecurityKey"];
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOcelot().AddKubernetes();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+app.MapControllers();
+
+// Add Authorization
+var config = new OcelotPipelineConfiguration
+{
+    AuthorizationMiddleware
+        = async (downStreamContext, next) =>
+        await JwtMiddleware.CreateAuthorizationFilter(downStreamContext, securityKey, next)
+};
+
+app.UseOcelot(config).Wait();
+
+app.Run();
